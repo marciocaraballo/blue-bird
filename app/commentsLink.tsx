0a182a53bc316f08c14843a5fbe3b2811d8738b7
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-08-07 03:47:27 -0300

    Fix comments icon

--- app/commentsLink.tsx
+++ app/commentsLink.tsx
@@ -4,25 +4,23 @@ import Link from 'next/link'
 
 export default function CommentsLink({ tweet }: { tweet: TweetWithAuthor }) {
     return (
-        <div>
-            <Link
-                className="text-gray-400 ml-2"
-                href={`/feed/comments/${tweet.id}`}
+        <Link
+            className="text-gray-400 ml-2"
+            href={`/feed/comments/${tweet.id}`}
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             >
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                >
-                    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-                </svg>
-            </Link>
-        </div>
-    )
+                <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
+            </svg>
+        </Link>
+)
 }
