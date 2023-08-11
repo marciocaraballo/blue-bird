37900645255cb8052f15d5bac2c2263665b82dd4
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-08-11 08:59:04 -0300

    I dont know anymore

--- app/components/Tweet.tsx
+++ app/components/Tweet.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Likes from '../feed/likes'
 import CommentsLink from '../commentsLink'
 import DateTime from './DateTime'
+import Delete from './Icons/Delete'
 
 export default function Tweet({
     tweet,
@@ -51,22 +52,7 @@ export default function Tweet({
                             className="ml-2"
                             onClick={() => handleDelete(tweet)}
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="fill-gray-400 hover:fill-red-600"
-                            >
-                                <polyline points="3 6 5 6 21 6"></polyline>
-                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                <line x1="10" y1="11" x2="10" y2="17"></line>
-                                <line x1="14" y1="11" x2="14" y2="17"></line>
-                            </svg>
+                            <Delete className="fill-gray-400 hover:fill-red-600"/>
                         </button>
                     ) : null}
                 </div>
